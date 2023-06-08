879ccd390238410f43bec215c43f6098385157c0
Xiran23 <thapachiran23@gmail.com>
2023-06-08 20:09:46 +0545

    without modified

--- navbar.php
+++ navbar.php
@@ -3,11 +3,11 @@
             <img src="images/logo.png" alt="logo" class="image">
         </div>
         <a href="#"> <i class="fa fa-desktop"> <span>Dashboard</span></i></a>
-        <a href="#" onclick="addProduct()"> <i class="fa fa-table"> <span>add-Product</span></i></a>
-        <a href="#" onclick="Viewproduct()"> <i class="fa fa-th">
+        <a href="addproduct.php" > <i class="fa fa-table"> <span>add-Product</span></i></a>
+        <a href="viewproduct.php" > <i class="fa fa-th">
                 <span>Viewproduct</span>
             </i></a>
-        <a href=""> <i class="fa fa-cogs"> <span>view</span></i></a>
+        <a href="viewcustomers.php"> <i class="fa fa-cogs"> <span>view users</span></i></a>
         <a href="#"> <i class="fa fa-info-circle"> <span>About</span></i></a>
         <a href="#"> <i class="fa fa-slider"> <span>Settings</span></i></a>
 
